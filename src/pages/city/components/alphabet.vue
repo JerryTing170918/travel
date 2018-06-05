2ce1ab94938b58347d813108d3be64d3672c7bbd
JerryTing170918 <564896052@qq.com>
2018-06-05 17:37:06 +0800

    city-components and 性能优化

--- src/pages/city/components/alphabet.vue
+++ src/pages/city/components/alphabet.vue
@@ -2,10 +2,15 @@
     <div>
         <ul class="list">
             <li class="item" 
-                v-for='(item, key) of cities' 
-                :key="key"
+                v-for='item of letters' 
+                :key="item"
+                :ref="item"
+                @touchstart="handleTouchStart"
+                @touchmove="handleTouchMove"
+                @touchend="handleTouchEnd"
+                @click='handleLetterClick'
             >
-                {{key}}
+                {{item}}
             </li>
         </ul>
     </div>
@@ -15,6 +20,54 @@ export default {
   name: "cityAlphabet",
   props:{
       cities:Object
+  },
+  computed:{
+      letters(){
+          const letters = []
+          for(let i in this.cities){
+              letters.push(i)
+          }
+          return letters
+      }
+  },
+  data(){
+      return{
+          touchStatus:false,
+          startY:0,
+          timer:null,
+
+      }
+  },
+  //性能优化
+  updated(){
+      this.startY = this.$refs['A'][0].offsetTop
+  },
+  methods:{
+      handleLetterClick(e){
+          this.$emit('change', e.target.innerText)
+      },
+    //   实现右侧字母表拖拽左侧跟着移动
+      handleTouchStart(){
+          this.touchStatus = true
+      },
+      handleTouchMove(e){
+          if(this.touchStatus){
+              if(this.timer){
+                  clearTimeout(this.timer)
+              }
+              this.timer = setTimeout(() => {
+                  const touchY = e.touches[0].clientY - 79
+                  const index = Math.floor((touchY - this.startY) / 20)
+                  if(index >= 0 && index < this.letters.length){
+                      this.$emit('change', this.letters[index])
+                  } 
+              }, 16)
+          }
+      },
+      handleTouchEnd(){
+          this.touchStatus = false
+      },
+
   }
 };
 </script>
