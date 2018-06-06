bdad8eb87a346208d70e99e9b95e2e091b9607f4
JerryTing170918 <564896052@qq.com>
2018-06-06 21:51:19 +0800

    common-gallary

--- src/pages/detail/components/banner.vue
+++ src/pages/detail/components/banner.vue
@@ -1,15 +1,41 @@
 <template>
-    <div class="banner">
+<div>
+    <div class="banner" @click="handleBannnerClick">
         <img class="banner-img" src="//img1.qunarzz.com/sight/p0/1508/12/4e6170303c63bdb7070fb1987480afab.water.jpg_600x330_8db91d9d.jpg" alt="">
         <div class="banner-info">
             <div class="banner-tittle">杭州极地海洋世界</div>
             <div class="banner-number"><span class="iconfont">&#xe629;</span>26</div>
         </div>
     </div>
+    <common-gallary 
+        :imgs='imgs' 
+        v-show='showGallary'
+        @close='handleGallaryClose'
+    ></common-gallary>
+</div>
+    
 </template>
 <script>
+import commonGallary from 'common/gallary/gallary'
 export default {
     name:'detailBanner',
+    data(){
+        return{
+            showGallary:false,
+            imgs:['http://img1.qunarzz.com/sight/p0/201403/07/0a7decf6c090404c1663ab2c88d69189.jpg_350x240_d419ec68.jpg','http://img1.qunarzz.com/sight/p0/201403/07/baf02b36fbd3e3405e28e27ab9598d26.jpg_350x240_e8612035.jpg']
+        }
+    },
+    methods:{
+        handleBannnerClick(){
+            this.showGallary = true
+        },
+        handleGallaryClose(){
+            this.showGallary = false
+        }
+    },
+    components:{
+        commonGallary
+    },
 }
 </script>
 <style lang="stylus" scoped>
